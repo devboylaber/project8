bde152ff69b35fcb0dd41c17458c28cde91dc98c
devboylaber <devboy.laber@gmail.com>
2014-11-18 17:25:03 -0500

    about to try cart

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20141117221325) do
     t.integer  "admin_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_file_name"
+    t.string   "image_file_name" 
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
