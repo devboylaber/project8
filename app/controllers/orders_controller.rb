f7f62d142efdd670e2fad1a1ae49b1c3bf9f19bd
devboylaber <devboy.laber@gmail.com>
2014-11-18 20:20:39 -0500

    juct chaneged orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,39 +28,24 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    @order = Order.new(order_params)
-    redirect_to cart_admin_order_url(@order)
-    # respond_to do |format|
-      if @order.save_with_payment
-        redirect_to @order, :notice => "Thank you for subscribing!"
-      else
-        render :new
-      end
-    # end
+    @order_info = current_order
+    @order = @order_info.orders.new(order_params)
+    @order_info.save
+    session[:order_id] = @order.id
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
   def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
+    @order_info = current_order
+    @order = @order_info.orders.find(params[:id])
+    @order.update_attributes(order_params)
+    @orders = @order_info.orders
   end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
   def destroy
+    @order_info = current_order
+    @order = @order_info.orders.find(params[:id])
     @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @orders = @order.orders
   end
 
   private
@@ -71,6 +56,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:product_id, :order_id, :user_id, :admin_id)
+      params.require(:order).permit(:product_id, :order_id, :unit_price, :quantity, :total_price, :user_id, :admin_id, :stripe_customer_token)
     end
 end
